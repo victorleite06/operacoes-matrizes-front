8004ce46bb6164cafe09870debe9513b5440ea77
DESKTOPLEITE\victo <victor.leite2001@gmail.com>
2024-05-25 15:28:32 -0300

    Header

--- src/app/components/appbar/appbar.component.ts
+++ src/app/components/appbar/appbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appbar',
   templateUrl: './appbar.component.html',
   styleUrl: './appbar.component.css'
 })
-export class AppbarComponent {
+export class AppbarComponent implements OnInit {
+  @Input() titulo: string = ''
 
+  ngOnInit(): void {
+
+  }
+
+  constructor() {}
 }
